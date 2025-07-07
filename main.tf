c5cd011dd1da6e41f4dc72d3e0ed3b63e616eb8b
apexsaksham <saksham26singh@gmail.com>
2025-07-07 15:42:24 +0530

    create seprate folder

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "ec2-backend-tf"        # your actual S3 bucket name
-    key    = "ec2/terraform.tfstate" # folder/file path in the bucket
-    region = "ap-south-1"
-
-  }
-}
 
 
-provider "aws" {
-  region = "ap-south-1"
-}
+
+
 
 resource "aws_instance" "my_server" {
   ami           = "ami-0f918f7e67a3323f0" #this is ami
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-0f918f7e67a3323f0"           #this is ami
+instance_type = "t2.micro"                        #this is instance type
