abc13322403fa786e2618483a257ad9fd895c28d
apexsaksham <saksham26singh@gmail.com>
2025-07-07 14:34:25 +0530

    clean start: terraform infra and destroy workflow

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "ec2-backend-tf"      # your actual S3 bucket name
+    key            = "ec2/terraform.tfstate"        # folder/file path in the bucket
+    region         = "ap-south-1"
+    
+  }
+}
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_server" {
+    ami = "ami-0f918f7e67a3323f0"            #this is ami
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "My_server"              # this is name
+    }
+  
+}
